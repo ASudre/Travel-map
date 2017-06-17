310261567ffab352d487828638e5d81cdc194719
Arthur Sudre <arthursudre@gmail.com>
2017-06-17 19:23:16 +0200

    fix(icons): add icons on production

--- public/Webpack/webpackRules.js
+++ public/Webpack/webpackRules.js
@@ -17,7 +17,7 @@ const cssLoaderRule = {
 
 const fontLoaderRule = {
     test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-    loader: 'file-loader?name=fonts/[name].[ext]',
+    loader: 'file-loader?name=fonts/[name].[ext]&publicPath=assets/fonts',
 };
 
 const eslintLoaderRule = {
